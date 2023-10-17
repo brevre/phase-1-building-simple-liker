8fc31c4d1cb3c47ceea59fe9e0a4b8ef33b0afb1
brevre <brendamakena09@gmail.com>
2023-10-17 07:35:21 +0200

    completed

--- main.js
+++ main.js
@@ -3,7 +3,47 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("modal");
+  const modalMessage = document.getElementById("modal-message");
 
+  // Add event listener to all like buttons
+  const likeButtons = document.querySelectorAll(".like");
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          // On success, change the heart icon and style
+          button.classList.add("activated-heart");
+          button.innerHTML = "Liked! <span class='like-glyph'>&#x2665;</span>";
+        })
+        .catch((error) => {
+          // On error, display the error modal
+          modalMessage.innerText = error;
+          modal.classList.remove("hidden");
+
+          // Hide the error modal after 3 seconds
+          setTimeout(() => {
+            modal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
+
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    // Simulate a server call (50% success, 50% failure)
+    setTimeout(() => {
+      const random = Math.random();
+      if (random < 0.5) {
+        resolve("Success");
+      } else {
+        reject("Error");
+      }
+    }, 300);
+  });
+}
 
 
 
